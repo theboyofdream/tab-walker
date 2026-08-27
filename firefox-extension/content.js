8b8e203d653116244e5fdb51586bdfdf023c9ce8
Developer <developer@example.com>
2026-08-27 16:13:43 +0530

    Change window height setting to max-height for dynamic overlay sizing

--- firefox-extension/content.js
+++ firefox-extension/content.js
@@ -19,7 +19,7 @@
     TOGGLE_WALKER: 'TOGGLE_WALKER'
   };
 
-  const fallbackFaviconSvg = `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="%234d5055" d="M12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12C2 6.48 6.48 2 12 2ZM4 12H8.4C11.81 12.02 13.32 13.73 12.94 17.13H9.49V19.6C13.34 19.89 16.88 18.35 19.29 15.32C19.83 14.13 20.07 12.82 19.99 11.52C19.33 12.5 18.33 13 17 13C14.86 13 13.79 12.08 13.79 10.25H10.04C9.77 7.52 10.72 6.16 12.91 6.16C12.91 5.19 13.24 4.56 13.72 4.19C10.18 4.21 6.99 5.77 4.79 8.54C4.27 9.62 4 10.8 4 12Z"/></svg>`;
+  const fallbackFaviconSvg = `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="%234d5055" d="M12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12C2 6.48 6.48 2 12 2ZM4 12H8.4C11.81 12.02 13.32 13.73 12.94 17.13H9.49V19.6C13.34 19.89 16.88 18.35 19.29 15.32C19.83 14.13 20.07 12.82 19.99 11.52C19.33 12.5 18.33 13 17 13C14.86 13 13.79 6.16 12.91 6.16C12.91 5.19 13.24 4.56 13.72 4.19C10.18 4.21 6.99 5.77 4.79 8.54C4.27 9.62 4 10.8 4 12Z"/></svg>`;
 
   // Create Host Element & Shadow DOM
   const host = document.createElement('div');
@@ -73,9 +73,8 @@
       box-shadow: 0 8px 32px rgba(0, 0, 0, 0.45);
       color: var(--card-color);
       width: var(--popup-width, 460px);
-      height: var(--popup-height, 500px);
       max-width: 90vw;
-      max-height: 85vh;
+      max-height: var(--popup-height, 500px);
       display: flex;
       flex-direction: column;
       overflow: hidden;
